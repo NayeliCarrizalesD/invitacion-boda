ad63e5d43b385d6f724eb44f696337fe22d7455e
Nayeli Carrizales <131297024+NayeliCarrizalesD@users.noreply.github.com>
2023-05-09 11:20:32 -0600

    cambios

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+  <div class="container">
+    <div class="navbar-translate">
+      <a class="navbar-brand" href="index.html">
+        Yaz & Jesús </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a href="views/login-page.html" class="nav-link" >
+            <i class="material-icons">favorites</i> Iniciar Sesion
+          </a>
+        </li>
+
+        
+      </ul>
+    </div>
+  </div>
+</nav>
